f084c855fd5c67e5cce701fde197c96234915b39
ROHIT RAY <rayrohit1080@gmail.com>
2022-03-25 23:23:48 +0530

    New changes

---
+++ BASIC.py
@@ -0,0 +1,50 @@
+#Please dont repeat it
+"""
+This is  amultiline comment
+
+"""
+"""
+This is a comment
+"""
+#print ("Rohit",end=" \nRay")
+#print("\nRohit is :  Agun")
+
+
+#variables
+
+var1="32"
+var2="46"
+#var3=(int(var1)+int(var2))
+
+#print(100*str(var3))
+"""
+str()
+int()
+float()
+"""
+#print("enter your Number")
+#inpnum=input()
+#print("Enter other Number")
+#inpnum1=input()
+#print(inpnum," + ",inpnum1," = ",int(inpnum)+int(inpnum1))
+#String slicing:
+mystr=("Rohit is a good boy:")
+print(mystr)
+print(mystr[0:10])
+
+mystr=("Rohit is a good boy:")
+print(len(mystr))
+print(mystr[0:10:2])
+
+mystr=("Rohit is a good boy:")
+print(len(mystr))
+print(mystr[-2:17])
+
+print(mystr.isalnum())
+print(mystr.endswith("boy:"))
+print(mystr.count("o"))
+print(mystr.capitalize())
+print(mystr.find("is"))
+print(mystr.upper())
+print(mystr.lower())
+print(mystr.replace("is","are"))
